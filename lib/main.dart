213fbd5e15d89f54786bdefb8ff9d63c200a5203
Rohit Pradeep Sudarsanan <rohit.psudarsanan@gmail.com>
2023-10-02 16:26:38 +0530

    constant achieved

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,16 +15,18 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-          title:const  Text('Responsive Cards'),
+          title: Text('Responsive Cards'),
         ),
-        body:const  MyCards(),
+        body: MyCards(),
       ),
     );
   }
 }
 
 class MyCards extends StatelessWidget {
-  const MyCards({super.key});
+  final double initialGreyAreaWidth = 100.0;
+  final double initialGreyAreaHeight = 50.0;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -47,7 +48,7 @@ class MyCards extends StatelessWidget {
                 child: Align(
                   alignment: Alignment.centerLeft,
                   child: FractionallySizedBox(
-                    widthFactor: .9, // Adjust as needed
+                    widthFactor: 0.9, // Adjust as needed
                     heightFactor: 0.2, // Maintain the same proportion
                     child: Container(
                       color: Colors.green,
@@ -74,21 +75,17 @@ class MyCards extends StatelessWidget {
                   children: [
                     Align(
                       alignment: Alignment.topCenter,
-                      child: FractionallySizedBox(
-                        widthFactor: 0.8, // Adjust as needed
-                        heightFactor:
-                            0.2, // Change the green area's height proportion
-                        child: Container(
-                          color: Colors.grey,
-                        ),
+                      child: Container(
+                        height: 50,
+                        width: 150,
+                        color: Colors.grey,
                       ),
                     ),
                     Align(
                       alignment: Alignment.bottomCenter,
                       child: FractionallySizedBox(
-                        widthFactor: 0.8, // Adjust as needed
-                        heightFactor:
-                            0.2, // Change the green area's height proportion
+                        widthFactor: 0.9, // Maintain the same proportion
+                        heightFactor: 0.2, // Adjust as needed
                         child: Container(
                           color: Colors.green,
                         ),
